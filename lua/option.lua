0adf65bb517948abf1af8d0efcc74d05e603e775
oncekey <once_key@126.com>
2023-02-27 18:31:31 +0800

    添加neo-tree

--- lua/option.lua
+++ lua/option.lua
@@ -1,5 +1,5 @@
-vim.g.encoding = 'UTF-8'
-vim.opt.fileencoding = 'utf-8'
+vim.g.encoding = "UTF-8"
+vim.opt.fileencoding = "utf-8"
 
 -- jkhl 移动时光标周围保留8行
 vim.o.scrolloff = 8
@@ -10,7 +10,7 @@ vim.opt.number = true
 -- 高亮所在行
 vim.opt.cursorline = true
 
-vim.wo.signcolumn = 'yes'
+vim.wo.signcolumn = "yes"
 
 vim.o.tabstop = 2
 vim.bo.tabstop = 2
@@ -22,26 +22,25 @@ vim.o.autoindent = true
 vim.bo.autoindent = true
 vim.o.smartindent = true
 vim.o.shiftwidth = 2
-vim.bo.shiftwidth =2
+vim.bo.shiftwidth = 2
 
-vim.o.background = 'dark'
+vim.o.background = "dark"
 vim.opt.termguicolors = true
 
 vim.o.list = true
-vim.o.listchars = 'space:·'
+--vim.o.listchars = "space:·"
 
-vim.o.showtabline = 2 
+vim.o.showtabline = 2
 
 -- 禁止创建备份文件
 vim.o.backup = false
 vim.o.writebackup = false
 vim.o.swapfile = false
 
+--vim.o.list = true
+--vim.o.listchars = "space: "
 
-vim.o.list = true
-vim.o.listchars = 'space:·'
-
-vim.o.guifont = "source Code Pro:h18"
+vim.o.guifont = "source Code Pro:h14"
 vim.opt.linespace = 0
 
 ------ neovide 配置
@@ -57,3 +56,4 @@ vim.g.neovide_cursor_animation_length = 0.10
 
 -- vim.g.neovide_cursor_vfx_mode = "wireframe"
 vim.g.neovide_cursor_vfx_mode = "sonicboom"
+
