6571fa67b01a629da40c3284a436e0f862e67995
oncekey <once_key@126.com>
2023-03-02 16:02:21 +0800

    formatter file

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,8 +1,8 @@
 ---@diagnostic disable: undefined-global
 local function map(mode, lhs, rhs, opts)
-  opts = opts or {}
-  opts.silent = opts.silent ~= false
-  vim.keymap.set(mode, lhs, rhs, opts)
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
 end
 
 map('i', '<C-f>', '<right>')
@@ -29,31 +29,32 @@ map('n', '<leader>gg', ':LazyGit<CR>')
 local P = {}
 
 local function map_lsp_keys()
-  map('n', 'gD', vim.lsp.buf.declaration)
-  map('n', 'gd', vim.lsp.buf.definition)
-  map('n', 'K', vim.lsp.buf.hover)
-  map('n', 'gi', vim.lsp.buf.implementation)
-  map('n', '<C-k>', vim.lsp.buf.signature_help)
-  map('n', '<space>D', vim.lsp.buf.type_definition)
-  map('n', '<space>rn', vim.lsp.buf.rename)
-  map('n', '<space>ca', vim.lsp.buf.code_action)
-  map('n', 'gr', require('telescope.builtin').lsp_references)
-  map('n', '<space>bf', function() vim.lsp.buf.format { async = true } end)
+    map('n', 'gD', vim.lsp.buf.declaration)
+    map('n', 'gd', vim.lsp.buf.definition)
+    map('n', 'K', vim.lsp.buf.hover)
+    map('n', 'gi', vim.lsp.buf.implementation)
+    map('n', '<C-k>', vim.lsp.buf.signature_help)
+    map('n', '<space>D', vim.lsp.buf.type_definition)
+    map('n', '<space>rn', vim.lsp.buf.rename)
+    map('n', '<space>ca', vim.lsp.buf.code_action)
+    map('n', 'gr', require('telescope.builtin').lsp_references)
+    map('n', '<space>bf', function() vim.lsp.buf.format { async = true } end)
 end
 
 
 function P.map_java_keys()
-  map_lsp_keys()
+    map_lsp_keys()
 
-  local spring_boot_run = 'mvn spring-boot:run -Dspring-boot.run.profiles=local'
-  local command = ':lua require("toggleterm").exec("' .. spring_boot_run .. '")<CR>'
+    local spring_boot_run = 'mvn spring-boot:run -Dspring-boot.run.profiles=local'
+    local command = ':lua require("toggleterm").exec("' .. spring_boot_run .. '")<CR>'
 
-  key_map('n', '<leader>rj', command)
-  key_map('n', '<leader>oi', ':lua require("jdtls".organize_imports()<CR>')
-  key_map('n', '<leader>jc', ':lua require("jdtls".compile("incremental")<CR>')
+    key_map('n', '<leader>rj', command)
+    key_map('n', '<leader>oi', ':lua require("jdtls".organize_imports()<CR>')
+    key_map('n', '<leader>jc', ':lua require("jdtls".compile("incremental")<CR>')
 end
 
 function P.map_lua_keys()
-  map_lsp_keys()
+    map_lsp_keys()
 end
+
 return P
