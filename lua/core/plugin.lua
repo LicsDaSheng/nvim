9aea2d0848f370746b2e8211c31ca157a329b90c
oncekey <once_key@126.com>
2023-03-02 11:36:30 +0800

    add gitsigns

--- lua/core/plugin.lua
+++ lua/core/plugin.lua
@@ -1,8 +1,8 @@
 local ensure_packer = function()
   local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
   if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
     vim.cmd [[packadd packer.nvim]]
     return true
   end
@@ -22,17 +22,17 @@ return require('packer').startup(function(use)
   }
   use {
     'nvim-neo-tree/neo-tree.nvim',
-     branch = 'v2.x',
-     requires = {
-       'nvim-lua/plenary.nvim',
-       'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-       'MunifTanjim/nui.nvim',
-     }
+    branch = 'v2.x',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+    }
   }
   use 'nvim-treesitter/nvim-treesitter'
   use {
-     'nvim-telescope/telescope.nvim', tag = '0.1.1',
-     requires = { {'nvim-lua/plenary.nvim'} }
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    requires = { { 'nvim-lua/plenary.nvim' } }
   }
 
   use 'kdheepak/lazygit.nvim'
@@ -42,11 +42,13 @@ return require('packer').startup(function(use)
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
   }
-
+  use {
+    'lewis6991/gitsigns.nvim',
+    tag = 'release',
+  }
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
     require('packer').sync()
   end
 end)
-
