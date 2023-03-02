58680de980163071148537578af8289901abb6ab
oncekey <once_key@126.com>
2023-03-02 11:45:20 +0800

    add indent_blankline

--- lua/core/plugin.lua
+++ lua/core/plugin.lua
@@ -44,8 +44,11 @@ return require('packer').startup(function(use)
   }
   use {
     'lewis6991/gitsigns.nvim',
-    tag = 'release',
+    --tag = 'release',
   }
+
+  use "lukas-reineke/indent-blankline.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
