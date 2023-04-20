336257388ed0ccc644a1b78808c85e1abf9bd54f
oncekey <once_key@126.com>
2023-04-20 13:47:59 +0800

    更改演示为gruvbox

--- config.lua
+++ config.lua
@@ -22,6 +22,7 @@ lvim.format_on_save = {
 require("user.mappings")
 -- -- Change theme settings
 -- lvim.colorscheme = "lunar"
+lvim.colorscheme = "gruvbox"
 
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
@@ -29,7 +30,6 @@ lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
-lvim.builtin.breadcrumbs.active = true
 lvim.builtin.treesitter.highlight.enabled = true
 lvim.builtin.dap.active = true
 
--- lua/user/plugin.lua
+++ lua/user/plugin.lua
@@ -1,5 +1,6 @@
 -- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
 lvim.plugins = {
+  "morhetz/gruvbox",
   {
     "folke/trouble.nvim",
     cmd = "TroubleToggle",
