794fb87fb3f173334e2856fcdfc2527398d931a7
oncekey <once_key@126.com>
2023-04-20 13:47:40 +0800

    修改golang的formatter

--- lua/user/formatters.lua
+++ lua/user/formatters.lua
@@ -2,5 +2,5 @@
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   { command = "goimports", filetypes = { "go" } },
-  { command = "gofmt",     filetypes = { "go" } },
+  { command = "gofumpt",   filetypes = { "go" } },
 }
