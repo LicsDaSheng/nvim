5be8ff6be6be876e3bd587f4e5a400102d379b20
oncekey <once_key@126.com>
2023-04-21 09:19:58 +0800

    添加ts autotag

--- lua/user/plugin.lua
+++ lua/user/plugin.lua
@@ -1,6 +1,11 @@
 -- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
 lvim.plugins = {
-  "morhetz/gruvbox",
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  }, "morhetz/gruvbox",
   {
     "folke/trouble.nvim",
     cmd = "TroubleToggle",
