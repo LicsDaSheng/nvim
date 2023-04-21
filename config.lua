8b728ed69da0bff430921cf1029cadaf0bc5dff4
oncekey <once_key@126.com>
2023-04-21 09:19:48 +0800

    修改lualine_a

--- config.lua
+++ config.lua
@@ -29,6 +29,7 @@ lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.lualine.sections.lualine_a = { 'mode' }
 
 lvim.builtin.treesitter.highlight.enabled = true
 lvim.builtin.dap.active = true
